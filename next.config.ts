edbf9ae97605f188d41e1a6d7e4158186496e0ee
Wakui2001 <wakuibase@gmail.com>
2025-10-01 16:55:29 +0900

    disable eslint during build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // Vercelでのビルド時にLintエラーを無視する設定
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
